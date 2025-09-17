36b1778a31533d12e86606f24246559c7d1e5c6f
Digits-Infotech-IT <digitsinfotechit@gmail.com>
2025-09-18 02:38:10 +0530

    initial code

---
+++ app.test.js
@@ -0,0 +1,11 @@
+describe('Sample Test', () => {
+	  test('should return true', () => {
+		      expect(true).toBe(true);
+		    });
+});
+describe('Sample Test', () => {
+	  test('should return true', () => {
+		      expect(true).toBe(true);
+		    });
+});
+
